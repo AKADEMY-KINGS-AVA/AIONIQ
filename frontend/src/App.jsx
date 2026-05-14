172662a3861d76a593323a882482e0d3dec43f04
ava <vados020676@gmail.com>
2026-05-14 23:46:25 +0300

    Создан анимированный bacground

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
-import Header from './components/Header'
 import Registration from './components/registration/Registration'
 import './App.css'
+
+import asset_1 from './assets/elips_1.svg'
+import asset_2 from './assets/elips_2.svg'
 export default function App() {
   return (
     <div className="App">
+      <img className="elips asset_1" src={asset_1} alt="" />
+      <img className="elips asset_2" src={asset_2} alt="" />
       <Registration />
     </div>
   )
