f78c6a6706909c2374c1b043c8a4b8a5ad0fdae3
ava <vados020676@gmail.com>
2026-05-17 18:10:52 +0300

    Добавлены приветственные соона фон

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,12 +16,18 @@ export default function App() {
       {isAuth ? <AioniqContent /> : <Registration setIsAuth={setIsAuth} />}
       {!isAuth && (
         <>
+          <h2 className="welcome-message-1">Мы рады </h2>
+          <h2 className="welcome-message-2" >приветствовать</h2>
+          <h2 className="welcome-message-3" >вас!</h2>
+          <h2 className="thank-you-message-1">Спасибо</h2>
+          <h2 className="thank-you-message-2">что выбрали на нас!</h2>
           <img className="logo asset_1" src={asset_1} alt="" />
           <img className="logo asset_2" src={asset_2} alt="" />
           <img className="logo asset_3" src={asset_3} alt="" />
           <img className="logo asset_4" src={asset_4} alt="" />
         </>
-      )}
-    </div>
+      )
+      }
+    </div >
   )
 }
